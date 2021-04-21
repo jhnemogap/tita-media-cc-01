2252de977215c5cd6e19c8a924928347499a1f0b
jhnemogap <jhnemogap@gmail.com>
2021-04-20 20:21:10 -0500

    [FEAT]: navigation list and change gallery mode -> in Main section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const [burgerMenuClassName, navBarBurger] = ["burger-icon", "nav-bar-burger"];
+const [galleryModeId1, galleryModeId2] = ["change-gallery-mode-1", "change-gallery-mode-2"];
+
+// ++++++  Change mode: main navigation menu  ++++++
 const $burgerIcon = document.getElementsByClassName(burgerMenuClassName)[0];
 
 $burgerIcon.onclick = () => {
@@ -6,3 +9,20 @@ $burgerIcon.onclick = () => {
   $menuList.classList.toggle("nav-bar-burger-toggle-on");
 }
 
+// ++++++  Change mode: gallery  ++++++
+const $galleryModeId1 = document.getElementById(galleryModeId1);
+const $galleryModeId2 = document.getElementById(galleryModeId2);
+
+const changeGalleryMode = (mode) => {
+  const nameOfClass = { mode1: "gallery-masonry__item", mode2: "gallery-masonry__item--single-row" };
+  let articles = Array.from(document.getElementsByTagName("article"));
+  if (nameOfClass[mode] !== articles[0].classList.value) {
+    articles.map((item) => {
+        item.classList.toggle(nameOfClass.mode1);
+        item.classList.toggle(nameOfClass.mode2);
+    })
+  }
+};
+
+$galleryModeId1.onclick = () => changeGalleryMode("mode1");
+$galleryModeId2.onclick = () => changeGalleryMode("mode2");
